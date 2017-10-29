2f079285a1dffc9c9ecbdd80d90bc2ac30200151
Foomie <oh.no.fumi23@gmail.com>
2017-10-29 16:01:00 +0900

    20171029_commit

---
+++ js/common.js
@@ -0,0 +1,7 @@
+$(function(){
+    $("#in_header").load("./common/header.html #ined_header"); 
+    $("#in_menu_list").load("./common/menu.html #menu_list"); 
+    $("#in_cpr").load("./common/footer.html #cpr"); 
+})
+
+//読み込み側id　対象パス　読み込まれる側id
